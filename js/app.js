0d045dd521060674bd60c0a6d6722c542d978fbc
Alexandre Dohrmann <alexandredohrmann@Alexandres-MacBook-Pro.local>
2018-06-13 23:48:52 -0600

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@
 	// 	console.log(i);
 	// }
 
-// Excited Kitten
+// Excited Kitten:
 // Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
 // For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
 // Hint: You will need to use Math.random()
@@ -28,3 +28,21 @@
 	// 	};
 
 //  *** I CAN'T FIGURE OUT THE ABOVE, SO I'M MOVING ON. :( ****
+
+
+// Fizz Buzz
+// NOTE: THIS IS A CLASSIC RITE-OF-PASSAGE FOR JAVASCRIPT PROGRAMMERS, ESPECIALLY ONES AT GA
+	// Write a javascript application that logs all numbers from 1 - 100.
+	// If a number is divisible by 3 log "Fizz" instead of the number.
+	// If a number is divisible by 5 log "Buzz" instead of the number.
+	// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+
+// for (let i = 1; i <= 100; i++) {
+// 	console.log(i);
+// 		if (i % 3 === 0) {
+// 			console.log("Fizz");
+// 		} else if (i % 5 === 0) {
+// 			console.log("Buzz");
+// 		} else (i % 3 === 0 && i % 5 === 0) 
+// 			console.log("FizzBuzz");
+// 	};
