de069fcd023e8688a665b996530fd6ccefa5abd4
Alexandre Dohrmann <alexandredohrmann@Alexandres-MacBook-Pro.local>
2018-06-13 23:40:40 -0600

    Commit 3 - Excited Kittens answered - but not fully. I couldn't understand the part about Math.random

--- js/app.js
+++ js/app.js
@@ -1,12 +1,30 @@
 // Easy Going:
 // 1. Write a for loop that will log the numbers 1 through 20.
-	for (let i=1; i <= 20; i++) {
-		console.log(i);
-	}
+	// for (let i=1; i <= 20; i++) {
+	// 	console.log(i);
+	// }
 
 // Get Even:
 // Write a for loop that will log only the even numbers in 0 through 200.
 // Hint: Think about the increment expression.
-	for (let i=0; i <= 200; i+=2) {
-		console.log(i);
-	}
+	// for (let i=0; i <= 200; i+=2) {
+	// 	console.log(i);
+	// }
+
+// Excited Kitten
+// Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+// For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+// Hint: You will need to use Math.random()
+
+	// for (let i = 1; i <= 20; i++) {
+	// 	console.log("Love me, pet me! HSSSSSS!");
+		
+	// 		let catTalkArr = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+	// 		function randomCatPhrase(arr) {
+	// 			if (arr[i] % 2 === 0) {
+	// 			console.log(Math.random(catTalkArr[i]));
+	// 			};
+	// 		};
+	// 	};
+
+//  *** I CAN'T FIGURE OUT THE ABOVE, SO I'M MOVING ON. :( ****
