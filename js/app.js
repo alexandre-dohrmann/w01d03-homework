2bf084dec1d2c2f443bdd6d8878ab3926c758a88
Alexandre Dohrmann <alexandredohrmann@Alexandres-MacBook-Pro.local>
2018-06-14 13:22:59 -0600

    Commit 7 - Kristyn and Thom have their outfits ready for class - array practice

--- js/app.js
+++ js/app.js
@@ -16,18 +16,24 @@
 // For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
 // Hint: You will need to use Math.random()
 
-/*let catTalkArr = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+// function randomCatResponse(catResponse) {
+//          return catResponse[Math.floor(Math.random()*catResponse.length)];
+// }
 
-	for (let i = 1; i <= 20; i++) {
-		console.log("Love me, pet me! HSSSSSS!");
-			function randomCatPhrase(arr) {
-				if (i % 2 === 0) {
-				console.log(Math.random(catTalkArr[i]));
-				};
-			};
-		};*/
+// let catResponse = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
 
-//  *** I CAN'T FIGURE OUT THE ABOVE, SO I'M MOVING ON. :( ****
+// console.log(randomCatResponse(catResponse));
+// console.log(randomCatResponse(catResponse));
+// console.log(randomCatResponse(catResponse));
+// console.log(randomCatResponse(catResponse));
+
+
+// for (let i = 1; i < 21; i++) {
+//     console.log("Love me, pet me! HSSSSSS!");
+//     if (i % 2 === 0) {
+//    console.log(randomCatResponse(catResponse));
+//     }
+// }
 
 
 // Fizz Buzz
@@ -72,8 +78,59 @@
 // Yell at the Ninja Turtles:
 	// Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
 	// Use a for loop to call toUpperCase() on each of them and print out the result.
-		let ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
-			for (i = 0; i < ninjaTurtles.length; i++) {
-				console.log(ninjaTurtles[i].toUpperCase());
-			}
+		// let ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+		// 	for (i = 0; i < ninjaTurtles.length; i++) {
+		// 		console.log(ninjaTurtles[i].toUpperCase());
+		// 	}
+
+
+// Return of the Closets: 
+	// Below, we've given you examples of Kristyn and Thom's closets modeled as data in JavaScript. Use this data to answer the following questions.
+
+		const kristynsCloset = [
+		  "left shoe",
+		  "cowboy boots",
+		  "right sock",
+		  "GA hoodie",
+		  "green pants",
+		  "yellow knit hat",
+		  "marshmallow peeps"
+		];
+
+	// // Thom's closet is more complicated. Check out this nested data structure!!
+		const thomsCloset = [
+		[
+		    // These are Thom's shirts
+		    "grey button-up",
+		    "dark grey button-up",
+		    "light blue button-up",
+		    "blue button-up",
+		  ],[
+		    // These are Thom's pants
+		    "grey jeans",
+		    "jeans",
+		    "PJs"
+		  ],[
+		    // Thom's accessories
+		    "wool mittens",
+		    "wool scarf",
+		    "raybans"
+		  ]
+		];
+	// Alien Attire
+	// Kristyn's left shoe has traveled through time and space and turned up in Thom's accessories drawer! 
+	// Remove Kristyn's shoe from the array and save it to the variable kristynsShoe. 
+		const kristynsShoe = kristynsCloset.splice(0,1);
+		console.log(kristynsShoe);
+	// Use that variable to add Kristyn's lost shoe to Thom's accessories array.
+		thomsCloset[2].push(kristynsShoe);
+		console.log(thomsCloset);
+	// Dress Us Up
+	// Modify your code to put together 3 separate outfits for Kristyn and Thom. 
+	// Put the output in a sentence to tell us what we'll be wearing. Mix and match!
+		console.log(`Kristyn is wearing ${kristynsCloset[1]}, ${kristynsCloset[2]}, and ${kristynsCloset[3]}.`);	
+		console.log(`Thom is wearing ${thomsCloset[0][1]}, ${thomsCloset[1][0]}, and ${thomsCloset[2][0]}.`);
+		console.log(`Thom is wearing ${thomsCloset[0][2]}, ${thomsCloset[1][1]}, and ${thomsCloset[2][2]}.`);	
+	
+
 
